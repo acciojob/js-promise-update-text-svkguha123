2b26df29d5b9830ba1846adc9dc4befb1cac0a24
Souvik Guha <svkguha123@gmail.com>
2023-04-02 11:46:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+
+function updateText() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+updateText()
+  .then((text) => {
+    document.getElementById("output").textContent = text;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
